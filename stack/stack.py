9ccf79e1ad0e576baf37fece6da0262db4a4807f
RichVI <richardwang2050@gmail.com>
2020-06-08 20:03:45 -0700

    MVP

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +11,62 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+
+#         # Array Structure
+#         self.storage = []
+
+#     def __len__(self):
+#         # Returns the length of array
+#         return self.size
+
+#     def push(self, value):
+#         # Increase the array size by 1
+#         self.size += 1
+#         # Add the new value to the top of the stack
+#         self.storage.append(value)
+
+
+#     def pop(self):
+#         # If array size is 0, then return None
+#         if self.size == 0:
+#             return None
+        
+#         # Else 
+#         # 1) Decrease the size of the array by 1
+#         # 2) Remove the item on the top of the stack
+#         else:
+#             self.size -= 1
+#             return self.storage.pop()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+
+        # Array Structure
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        # Returns the length of array
+        return self.size
 
     def push(self, value):
-        pass
+        # Increase the array size by 1
+        self.size += 1
+        # Add the new value to the tail of the linkedlist
+        self.storage.add_to_tail(value)
+
 
     def pop(self):
-        pass
+        # If array size is 0, then return None
+        if self.size == 0:
+            return None
+        
+        # Else 
+        # 1) Decrease the size of the array by 1
+        # 2) Remove the tail from the linkedlist
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
